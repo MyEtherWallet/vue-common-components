5418c0be64aae5c266f35339800af76b69f07570
olgakup <olga.kupchevskaya.812@my.csun.edu>
2024-07-01 14:42:26 -0700

    devop: add fotoer export

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,6 +1,6 @@
 export type AmplitudeTrackType = (
   event: string,
-  options?: { route: string }
+  options?: { route: string, item?: string, }
 ) => void;
 export interface AmplitudePropType {
   track: AmplitudeTrackType;
