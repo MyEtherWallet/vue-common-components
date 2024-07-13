3e84a62fbf5d38986de67d6ff1bd2c09143cc86d
olgakup <olga.kupchevskaya.812@my.csun.edu>
2024-07-13 12:13:54 -0700

    devop: ensurte styling in costom component

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({ customElement: true })],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
