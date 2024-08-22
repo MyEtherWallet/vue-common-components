77bf206fe755394c63960b96c372842de9d64ade
olgakup <olga.kupchevskaya.812@my.csun.edu>
2024-08-22 14:53:09 -0700

    devop: add width and height

--- src/libs/layouts/MewSubscribe.vue
+++ src/libs/layouts/MewSubscribe.vue
@@ -35,7 +35,7 @@
             </div>
             <img :src="IMGPeggy" alt=""
                 class="sm:self-end sm:justify-self-center object-contain order-1 sm:order-2 mx-auto h-[200px] pt-20 sm:h-auto sm:pl-14"
-                width="180px" height="auto" loading="lazy" />
+                width="180px" height="270" loading="lazy" />
         </div>
 
         <!-- The backdrop, rendered as an absolute sibling to the panel container -->
@@ -105,9 +105,9 @@
                                                     id="checkboxCryptoKb" />
                                                 <span
                                                     class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
-                                                        viewBox="0 0 20 20" fill="currentColor" stroke="currentColor"
-                                                        stroke-width="1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" width="20"
+                                                        height="20" viewBox="0 0 20 20" fill="currentColor"
+                                                        stroke="currentColor" stroke-width="1">
                                                         <path fill-rule="evenodd"
                                                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                                                             clip-rule="evenodd"></path>
@@ -130,9 +130,9 @@
                                                     id="checkBoxMarket" />
                                                 <span
                                                     class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
-                                                        viewBox="0 0 20 20" fill="currentColor" stroke="currentColor"
-                                                        stroke-width="1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" width="20"
+                                                        height="20" viewBox="0 0 20 20" fill="currentColor"
+                                                        stroke="currentColor" stroke-width="1">
                                                         <path fill-rule="evenodd"
                                                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                                                             clip-rule="evenodd"></path>
@@ -173,7 +173,7 @@
 
                                 <svg v-if="isLoading" aria-hidden="true"
                                     class="w-8 h-8 text-white-40 animate-spin fill-white" viewBox="0 0 100 101"
-                                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    width="32" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
                                         fill="currentColor" />
